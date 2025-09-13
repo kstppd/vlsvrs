45a7d22b97e756b7105db2bfc6275e5d1935cead
kstppd <kpapadakis@protonmail.com>
2025-09-13 15:27:57 +0300

    better inputs

--- src/vlsv_ptr_gc2d.rs
+++ src/vlsv_ptr_gc2d.rs
@@ -5,6 +5,9 @@ use crate::mod_vlsv_tracing::*;
 use crate::vlsv_reader::*;
 use rayon::iter::ParallelIterator;
 use rayon::prelude::*;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::sync::{Arc, Mutex};
 
 pub fn push_gc_population_cpu_adpt<T: PtrTrait, F: Field<T> + Sync>(
@@ -42,37 +45,75 @@ pub fn push_gc_population_cpu_adpt<T: PtrTrait, F: Field<T> + Sync>(
 }
 
 fn main() -> Result<std::process::ExitCode, std::process::ExitCode> {
+    let args: Vec<String> = env::args().collect();
     let fields = VlsvDynamicField::<f64>::new(&String::from(
         "/wrk-vakka/group/spacephysics/vlasiator/2D/AID/bulk/",
     ));
     let mass = physical_constants::f64::PROTON_MASS;
     let charge = physical_constants::f64::PROTON_CHARGE;
-    let energy_kev = 112.0;
-    let pitch_angle_deg: f64 = 90.0;
-
-    let ke_joules = energy_kev * 1.0e3 * physical_constants::f64::EV_TO_JOULE;
-    let v = (2.0 * ke_joules / mass).sqrt();
-    let pitch = pitch_angle_deg.to_radians();
-    let vpar = v * pitch.cos();
-    let vperp = v * pitch.sin();
     let mut actual_time: f64 = 0.0;
-
     let mut pop = GCPopulation::new(mass, charge);
-    for L in 8..=10 {
-        let r = (L as f64) * physical_constants::f64::EARTH_RE;
-        let fields_here = fields.get_fields_at(actual_time, r, 0.0, 0.0).unwrap();
-        let bmag = mag(fields_here[0], fields_here[1], fields_here[2]);
-        let mu = 0.5 * mass * vperp * vperp / bmag;
 
-        pop.add(GuidingCenter2D {
-            x: r,
-            y: 0.0,
-            vpar,
-            mu,
-            alive: true,
-            dt: 1e-2,
-        });
+    if args.len() > 1 {
+        let filename = &args[1];
+        let file = File::open(filename).expect("Failed to open input file");
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if line.trim().is_empty() {
+                continue;
+            }
+            let sub: Vec<f64> = line
+                .split(',')
+                .map(|s| s.trim().parse::<f64>().expect("Nan ???"))
+                .collect();
+
+            if sub.len() != 6 {
+                panic!("Wrong format: {}", line);
+            }
+            let (time, x, y, z, vx, vy, vz) =
+                (sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6]);
+            actual_time = time;
+            let fields_here = fields.get_fields_at(actual_time, x, y, z).unwrap();
+            let bmag = mag(fields_here[0], fields_here[1], fields_here[2]);
+            let vperp2 = vx * vx + vy * vy;
+            let mu = 0.5 * mass * vperp2 / bmag;
+            pop.add(GuidingCenter2D {
+                x,
+                y,
+                vpar: vz,
+                mu,
+                alive: true,
+                dt: 1e-2,
+            });
+        }
+    } else {
+        //Test-Default
+        let energy_kev = 112.0;
+        let pitch_angle_deg: f64 = 90.0;
+        let ke_joules = energy_kev * 1.0e3 * physical_constants::f64::EV_TO_JOULE;
+        let v = (2.0 * ke_joules / mass).sqrt();
+        let pitch = pitch_angle_deg.to_radians();
+        let vpar = v * pitch.cos();
+        let vperp = v * pitch.sin();
+
+        for L in 8..=10 {
+            let r = (L as f64) * physical_constants::f64::EARTH_RE;
+            let fields_here = fields.get_fields_at(actual_time, r, 0.0, 0.0).unwrap();
+            let bmag = mag(fields_here[0], fields_here[1], fields_here[2]);
+            let mu = 0.5 * mass * vperp * vperp / bmag;
+
+            pop.add(GuidingCenter2D {
+                x: r,
+                y: 0.0,
+                vpar,
+                mu,
+                alive: true,
+                dt: 1e-2,
+            });
+        }
     }
+
     let mut pop = Arc::new(Mutex::new(pop));
     for out in 0..500 {
         push_gc_population_cpu_adpt(&mut pop, &fields, 5.0, &mut actual_time);
