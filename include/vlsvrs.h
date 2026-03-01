2f7847532e326061bcb46a4806e6b00487f4ce43
kstppd <kpapadakis@protonmail.com>
2026-03-01 11:25:45 +0200

    indendt

--- include/vlsvrs.h
+++ include/vlsvrs.h
@@ -51,18 +51,18 @@ typedef struct {
 } VLSVRS_Grid64;
 
 extern "C"{
-VLSVRS_GenericGrid read_var(const char *fname, const char *varname, int op);
-VLSVRS_Grid32 read_var_32(const char *fname, const char *varname, int op);
-VLSVRS_Grid64 read_var_64(const char *fname, const char *varname, int op);
-VLSVRS_GenericGrid read_vdf(const char *fname, const char *pop, size_t cid);
-VLSVRS_Grid32 read_vdf_32(const char *fname, const char *pop, size_t cid);
-VLSVRS_Grid64 read_vdf_64(const char *fname, const char *pop, size_t cid);
-void read_vdf_into_32(const char *fname, const char *pop, size_t cid,
-                      VLSVRS_Grid32 *target);
-void read_vdf_into_64(const char *fname, const char *pop, size_t cid,
-                      VLSVRS_Grid64 *target);
-double read_scalar_parameter(const char *fname, const char *parameter);
-size_t get_wid(const char *fname, const char *pop);
+  VLSVRS_GenericGrid read_var(const char *fname, const char *varname, int op);
+  VLSVRS_Grid32 read_var_32(const char *fname, const char *varname, int op);
+  VLSVRS_Grid64 read_var_64(const char *fname, const char *varname, int op);
+  VLSVRS_GenericGrid read_vdf(const char *fname, const char *pop, size_t cid);
+  VLSVRS_Grid32 read_vdf_32(const char *fname, const char *pop, size_t cid);
+  VLSVRS_Grid64 read_vdf_64(const char *fname, const char *pop, size_t cid);
+  void read_vdf_into_32(const char *fname, const char *pop, size_t cid,
+                        VLSVRS_Grid32 *target);
+  void read_vdf_into_64(const char *fname, const char *pop, size_t cid,
+                        VLSVRS_Grid64 *target);
+  double read_scalar_parameter(const char *fname, const char *parameter);
+  size_t get_wid(const char *fname, const char *pop);
 }
 #ifdef VLSVRS_STRIP_PREFIX
 #define VLSVRS_Grid32 Grid32
